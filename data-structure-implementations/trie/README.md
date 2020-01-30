19e44de7537c6170c84dcc745dd0e1629041bebb
Rajdeep <rajdeeps.iitb@gmail.com>
2020-01-30 23:28:44 +0900

    Search pattern implemented and unit tested

---
+++ data-structure-implementations/trie/README.md
@@ -0,0 +1,7 @@
+## Prefix Tree or Trie
+
+### Insert a word
+
+### Search a word
+
+### Find words which start with a string pattern 
--- data-structure-implementations/trie/trie_test.go
+++ data-structure-implementations/trie/trie_test.go
@@ -13,9 +13,16 @@ func TestStack(t *testing.T) {
     trie  := CreateNewTrie();
 	trie.InsertAWord("abc")
     trie.InsertAWord("abcd")
+    trie.InsertAWord("abcdd")
     
 	assert.True(t, trie.SearchIfWordExists("abc"))
     assert.True(t, trie.SearchIfWordExists("abcd"))
-    assert.False(t, trie.SearchIfWordExists("hello"))
-
+    assert.False(t, trie.SearchIfWordExists("helloworld"))
+    
+    trie.InsertAWord("helloworld")
+    assert.True(t, trie.SearchIfWordExists("helloworld"))
+    
+    assert.Equal(t, 3, len(trie.SearchWordsMatchingPattern("abc")), "trie.SearchWordsMatchingPattern")
+    assert.Equal(t, 1, len(trie.SearchWordsMatchingPattern("hello")), "trie.SearchWordsMatchingPattern")
+    assert.Equal(t, 0, len(trie.SearchWordsMatchingPattern("world")), "trie.SearchWordsMatchingPattern")
 }
